c1706f7cff308c64a5606d82b52f45b6bf0f0b56
alkingbibi <94806216+alkingbibi@users.noreply.github.com>
2026-02-20 01:35:16 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 import 'features/auth/login_page.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Initialize Firebase only if no apps exist yet
-  if (Firebase.apps.isEmpty) {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-  }
-
-  // Catch Flutter runtime errors globally
+void main() {
+  // التقاط أي خطأ أثناء تشغيل التطبيق
   FlutterError.onError = (FlutterErrorDetails details) {
     FlutterError.presentError(details);
     runApp(ErrorApp(details.exceptionAsString()));
@@ -33,12 +22,12 @@ class LostAndFoundApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: LoginPage(), // بدون const
+      home: const LoginPage(),
     );
   }
 }
 
-// Error screen to show runtime errors
+// شاشة عرض الخطأ
 class ErrorApp extends StatelessWidget {
   final String errorMessage;
   const ErrorApp(this.errorMessage, {super.key});
